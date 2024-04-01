337cc94f25fc8b38ded22ea037dcbc59f8168f30
cleveng <cleveng@gmail.com>
2024-04-01 18:57:00 +0800

    fix typo

--- docs/src/.vitepress/config/zh.ts
+++ docs/src/.vitepress/config/zh.ts
@@ -149,7 +149,7 @@ function sidebarGuide(): DefaultTheme.SidebarItem[] {
                 {text: 'Docker', link: 'docker'},
                 {text: 'Swarm', link: 'swarm'},
                 {text: 'K8s', link: 'k8s'},
-                {text: 'System', link: 'system'},
+                {text: 'Systemd', link: 'systemd'},
             ]
         },
         {
